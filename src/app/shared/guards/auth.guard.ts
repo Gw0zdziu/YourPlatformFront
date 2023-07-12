ccd557b38a000331b3a5b04b29ca510459087818
Gw0zdziu <gw0zdziu09@gmail.com>
2023-07-12 22:47:33 +0200

    Add auth guard

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,4 +1,4 @@
-import {ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot} from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
 import {AuthService} from "../services/http/auth/auth.service";
 import {inject} from "@angular/core";
 

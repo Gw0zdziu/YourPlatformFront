7187ad581b7f4f42d006c669a9159da4d795cafe
przemyslawgwozdz <gw0zdziu09@gmail.com>
2024-02-04 19:30:17 +0100

    add reused component to use if list is empty

---
+++ src/app/shared/components/empty-list/empty-list.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-empty-list',
+  templateUrl: './empty-list.component.html',
+  styleUrls: ['./empty-list.component.css']
+})
+export class EmptyListComponent {
+  @Input({required: true}) emptyListMessage: string;
+
+}
